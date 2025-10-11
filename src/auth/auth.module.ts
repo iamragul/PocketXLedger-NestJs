41952b745f61462a9e9709b65fee9e8181a6ff72
iamragul <mail@ragul.me>
2025-10-11 22:59:41 +0530

    fix: update getHello method to return an object and add Public decorator to the route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { ClerkClientProvider } from 'src/providers/clerk-client.provider';
+
+import { ClerkStrategy } from './clerk.strategy';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'clerk' })],
   controllers: [],
-  providers: [],
+  providers: [ClerkStrategy, ClerkClientProvider],
   exports: [PassportModule],
 })
 export class AuthModule {}
